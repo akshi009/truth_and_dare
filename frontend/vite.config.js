b0771e6bd32dadbc9f16d25c3846a976f86a2c30
akshi009 <akshi20jain03@gmail.com>
2025-06-05 23:57:10 +0530

    deploying

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,15 +4,12 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig(({ mode }) => {
   // Load env variables depending on mode (development, production)
-  const env = loadEnv(mode, process.cwd(), '');
-  const VITE_API_BACKEND_URL = env.VITE_API_BACKEND_URL;
-
   return {
     plugins: [react(), tailwindcss()],
     server: {
       proxy: {
         '/api': {
-          target: VITE_API_BACKEND_URL,
+          target: 'https://truth-and-dare-backend-pmj6ougik-akshi009s-projects.vercel.app',
           changeOrigin: true,
           rewrite: (path) => path.replace(/^\/api/, ''),
         },
