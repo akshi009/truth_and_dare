3c25532f72450ca0c6f00ccbb52feb0f2581575b
akshi009 <akshi20jain03@gmail.com>
2025-06-05 17:20:20 +0530

    done

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,22 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000', // Your Express backend
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+export default defineConfig(({ mode }) => {
+  // Load env variables depending on mode (development, production)
+  const env = loadEnv(mode, process.cwd(), '');
+  const VITE_API_BACKEND_URL = env.VITE_API_BACKEND_URL;
+
+  return {
+    plugins: [react(), tailwindcss()],
+    server: {
+      proxy: {
+        '/api': {
+          target: VITE_API_BACKEND_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
       },
     },
-  },
-})
+  }
+});
